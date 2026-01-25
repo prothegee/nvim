f613b2799b973ef63cdf37beafb1cd5c5093563f
prothegee <prothegee@proton.me>
2026-01-25 17:34:26 +0700

    init required settings

---
+++ lua/settings/init.lua
@@ -0,0 +1,11 @@
+require("settings.lsp")
+require("settings.treesitter")
+
+require("settings.global")
+require("settings.option")
+require("settings.diagnostic")
+require("settings.commands")
+require("settings.keymaps")
+require("settings.theme")
+
+require("settings.autocmd")
