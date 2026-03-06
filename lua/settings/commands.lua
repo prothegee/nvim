989240692e83a005d1fbc50aca348d29fe0d2c64
prothegee <prothegee@proton.me>
2026-03-06 16:17:15 +0700

    adjusting _ "underscore"

--- lua/settings/commands.lua
+++ lua/settings/commands.lua
@@ -1,9 +1,4 @@
---[[
-note:
-* only vim pack related
---]]
-
-local _cmd = {
+local cmd = {
     nvim_pack_add = "NvimPackAdd",
     nvim_pack_list = "NvimPackList",
     nvim_pack_update = "NvimPackUpdate",
@@ -17,34 +12,35 @@ local _cmd = {
 }
 
 
-vim.api.nvim_create_user_command(_cmd.nvim_pack_add, function(opts)
+vim.api.nvim_create_user_command(cmd.nvim_pack_add, function(opts)
     vim.notify("TODO: NvimPackAdd " .. tostring(#opts.fargs) .. " args")
 end, { nargs = "*" })
 
-vim.api.nvim_create_user_command(_cmd.nvim_pack_list, function()
+vim.api.nvim_create_user_command(cmd.nvim_pack_list, function()
     vim.notify("TODO: NvimPackList")
 end, {})
 
-vim.api.nvim_create_user_command(_cmd.nvim_pack_update, function()
+vim.api.nvim_create_user_command(cmd.nvim_pack_update, function()
     vim.pack.update()
 end, {})
 
-vim.api.nvim_create_user_command(_cmd.nvim_pack_remove, function(opts)
+vim.api.nvim_create_user_command(cmd.nvim_pack_remove, function(opts)
     vim.notify("TODO: NvimPackRemove " .. tostring(#opts.fargs) .. " args")
 end, { nargs = "*" })
 
+
 -- show all diagnostics in quickfix list
-vim.api.nvim_create_user_command(_cmd.diagnostic_show_all, function()
+vim.api.nvim_create_user_command(cmd.diagnostic_show_all, function()
         vim.diagnostic.setqflist({ open = true })
 end, { desc = "Show all diagnostics in quickfix list" })
 
 -- show diagnostic in float window
-vim.api.nvim_create_user_command(_cmd.diagnostic_show_float_window, function()
+vim.api.nvim_create_user_command(cmd.diagnostic_show_float_window, function()
         vim.diagnostic.open_float({ scope = "cursor" })
 end, { desc = "Show diagnostic message in float window" })
 
 -- diagnostic text
-vim.api.nvim_create_user_command(_cmd.diagnostic_toggle_virt_text, function()
+vim.api.nvim_create_user_command(cmd.diagnostic_toggle_virt_text, function()
     local cfg = vim.diagnostic.config() or {}
     local virt_text = cfg.virtual_text ~= false
 
@@ -54,7 +50,7 @@ vim.api.nvim_create_user_command(_cmd.diagnostic_toggle_virt_text, function()
 end, { desc = "Toggle diagnostic virtual text" })
 
 -- diagnostic lines
-vim.api.nvim_create_user_command(_cmd.diagnostic_toggle_virt_line, function()
+vim.api.nvim_create_user_command(cmd.diagnostic_toggle_virt_line, function()
     local cfg = vim.diagnostic.config() or {}
     local virt_lines = cfg.virtual_lines ~= false
 
@@ -64,7 +60,7 @@ vim.api.nvim_create_user_command(_cmd.diagnostic_toggle_virt_line, function()
 end, { desc = "Toggle diagnostic virtual line" })
 
 -- diagnostic text and lines
-vim.api.nvim_create_user_command(_cmd.diagnostic_toggle_virt_text_and_line, function()
+vim.api.nvim_create_user_command(cmd.diagnostic_toggle_virt_text_and_line, function()
     local cfg = vim.diagnostic.config() or {}
     local virt_txt = cfg.virtual_text ~= false
     local virt_line = virt_txt and true or false
