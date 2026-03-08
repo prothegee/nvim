55d60048d8c3b2d2a3e0fdfd3a8a1190bb7dbfbc
prothegee <prothegee@proton.me>
2026-03-08 19:12:48 +0700

    hidden for now

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,15 +1,15 @@
-vim.pack.add({"git@github.com:ibhagwan/fzf-lua.git"})
-
-local this = require"fzf-lua"
-
-this.setup({
-    buffers = {
-        sort_lastused = false
-    },
-    files = {
-        hidden = true,
-        rg_opts = [[--no-ignore --hidden --files -g "!*.git" --sort-files]],
-        fd_opts = [[--no-ignore --hidden --type f --type l --exclude .git --sort=name]],
-        cwd_prompt = false,
-    }
-})
+-- vim.pack.add({"git@github.com:ibhagwan/fzf-lua.git"})
+--
+-- local this = require"fzf-lua"
+--
+-- this.setup({
+--     buffers = {
+--         sort_lastused = false
+--     },
+--     files = {
+--         hidden = true,
+--         rg_opts = [[--no-ignore --hidden --files -g "!*.git" --sort-files]],
+--         fd_opts = [[--no-ignore --hidden --type f --type l --exclude .git --sort=name]],
+--         cwd_prompt = false,
+--     }
+-- })
