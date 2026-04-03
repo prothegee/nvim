3d363493ca558bf630190c06c5e99745027408d1
prothegee <prothegee@proton.me>
2026-04-04 02:15:03 +0700

    use global too

--- lua/settings/treesitter.lua
+++ lua/settings/treesitter.lua
@@ -6,32 +6,8 @@ local path_parser = vim.fn.stdpath("data") .. "/site/parser"
 
 ---
 
--- for some reason, this not loaded from fresh install
-M.TS = {
-    "lua",
-    "c", "cpp", "cmake",
-    "rust",
-    "zig", "ziggy", "ziggy_schema",
-    "c_sharp",
-    "go",
-    "java", "kotlin",
-    "ruby",
-    "javascript", "typescript",
-    "svelte", "vue",
-    "gdscript", "gdshader",
-    "dart",
-    "python",
-    "html", "css", "scss", -- "drogon-csp",
-    "json", -- "jsonc", "json5",
-    "markdown", "typst",
-    "yaml", "toml",
-    "bash",
-    "sql",
-    "dockerfile",
-}
-
 ts.setup({
-    ensure_installed = M.TS,
+    ensure_installed = _G._prt_TS,
     auto_install = true,
     sync_install = true, -- ensured install all first
     highlight = {
@@ -42,16 +18,18 @@ ts.setup({
 
 ---
 
-vim.filetype.add(M.TS)
-for _, lang in ipairs(M.TS) do
+vim.filetype.add(_G._prt_TS)
+for _, lang in ipairs(_G._prt_TS) do
     vim.treesitter.language.add(lang)
 
-    local parser = path_parser .. "/" .. lang .. ".so"
-    -- since ts.setup not installing the lang parser,
-    -- force add the parser if not exists
-    if vim.fn.filereadable(parser) == 0 then
-        vim.cmd("TSInstall " .. lang)
-    end
+    vim.schedule(function()
+        local parser = path_parser .. "/" .. lang .. ".so"
+        -- since ts.setup not installing the lang parser,
+        -- force add the parser if not exists
+        if vim.fn.filereadable(parser) == 0 then
+            vim.cmd("TSInstall " .. lang)
+        end
+    end)
 end
 
 ---
