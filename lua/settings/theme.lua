a246110ead6f4ba8178cb513640837474e6f03a6
prothegee <prothegee@proton.me>
2026-04-09 02:57:57 +0700

    adjust pumwindow

--- lua/settings/theme.lua
+++ lua/settings/theme.lua
@@ -50,8 +50,16 @@ vim.cmd([[
 
 vim.opt.statusline = "  %{v:lua.get_active_current_mode()}   %{v:lua.get_trim_path_current_buffer(1)} %=  %{v:lua.get_active_lsp()} 󰊢 %{v:lua.get_git_branch()} %{v:lua.get_git_short()}   %{v:lua.get_diagnostic_hint()}  %{v:lua.get_diagnostic_info()}  %{v:lua.get_diagnostic_warn()}  %{v:lua.get_diagnostic_error()}  L%l:C%c  󱗖 %p%% "
 
-vim.api.nvim_set_hl(0, "Pmenu", { bg = "#121212", fg = "#909090" })
-vim.api.nvim_set_hl(0, "PmenuExtra", { bg = "#121212", fg = "#909090" })
+vim.opt.pumheight = 24
+-- vim.opt.pummaxheight = 12
+vim.opt.pumblend = 0
+-- vim.opt.pumwidth = 48
+vim.opt.pummaxwidth = 75
+-- "single", "double", "rounded", "bold"
+vim.opt.pumborder = "rounded"
+
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "none", fg = "#606060" })
+vim.api.nvim_set_hl(0, "PmenuExtra", { bg = "none", fg = "#606060" })
 
 vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#4b4b4b", fg = "#eaeaea" })
 vim.api.nvim_set_hl(0, "PmenuExtraSel", { bg = "#4b4b4b", fg = "#eaeaea" })
@@ -63,4 +71,4 @@ vim.api.nvim_set_hl(0, "PmenuSbar", { bg = "#242424", fg="red" })
 
 vim.api.nvim_set_hl(0, "PmenuThumb", { bg = "#4b4b4b" })
 
-vim.api.nvim_set_hl(0, "PmenuBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "PmenuBorder", { bg = "none", fg="#1a8712" })
